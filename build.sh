e5502e6d3e0b33b690bdbf5f3fb669c0596f7fa1
noobwang233 <1213236870@qq.com>
2024-04-28 18:08:00 +0800

    add atk board

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 make distclean
 # bear -- make O=out mx6ull_alientek_emmc_defconfig
-bear -- make O=out mx6ull_14x14_evk_emmc_defconfig
+bear -- make O=out mx6ull_atk_emmc_defconfig
 bear -- make O=out -j8
 # rm ~/OSD_share/u-boot.imx
 cp out/u-boot-dtb.imx ~/OSD_share/
